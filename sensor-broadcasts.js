c397c51092cbb1da782a2838cfba37bb33932f14
Joseph Weigl <2631895+eloo@users.noreply.github.com>
2018-11-25 16:30:36 +0100

    Update sensor-broadcasts.js

--- sensor-broadcasts.js
+++ sensor-broadcasts.js
@@ -1,4 +1,4 @@
-const ENABLE_BATTERY_VOLTAGE = false;
+const ENABLE_BATTERY_VOLTAGE = true;
 const ENABLE_TEMPERATURE = true;
 const ENABLE_LIGHT_SENSOR = false;
 const ENABLE_CAP_SENSE = false;
@@ -9,68 +9,52 @@ const INTERVAL = 5000;
 
 // helper function for publishing messages
 function MQTT_publish(topic, message) {
-    NRF.setAdvertising({0x0590 : message});
+  NRF.setAdvertising({0x0590 : message});
   console.log(topic + ": " + message);
 }
 
 function getBatteryVoltage() {
-  if(!ENABLE_BATTERY_VOLTAGE) {
-    return 0;
-  }
-
   return NRF.getBattery();
 }
 
 function getLight(){
-  if(!ENABLE_LIGHT_SENSOR){
-    return 0;
-  }
   return Puck.light();
 }
-
-
-function encodeCapSense() {
-  if(!ENABLE_CAP_SENSE) {
-    return 0;
-  }
-  return Puck.capSense();
-}
-
 
 function getMagnetometer() {
   var mag = [ 0, 0, 0];
 
-  if(ENABLE_MAGNETOMETER) {
     var readings = Puck.mag();
 
     mag[0] = readings.x;
     mag[1] = readings.y;
     mag[2] = readings.z;
-  }
 
   return mag;
 }
 
 function getTemperature(){
-  if(!ENABLE_TEMPERATURE){
-    return 0;
-  }
   return E.getTemperature();
 }
 
 function publishData(){
-  NRF.setAdvertising({},{manufacturer: 0x0590, manufacturerData:[presses]});
-  MQTT_publish('home-assistant/sensor/puck/temp',getTemperature());
-  MQTT_publish('home-assistant/sensor/puck/batt', getBatteryVoltage());
-  MQTT_publish('home-assistant/sensor/puck/light', getLight());
-  MQTT_publish('home-assistant/sensor/puck/mag', getMagnetometer());
+  if(ENABLE_TEMPERATURE){
+    MQTT_publish('home-assistant/sensor/puck/temp',getTemperature());
+  }
+  if(ENABLE_BATTERY_VOLTAGE) {
+    MQTT_publish('home-assistant/sensor/puck/batt', getBatteryVoltage());
+  }
+  if(ENABLE_LIGHT_SENSOR){
+    MQTT_publish('home-assistant/sensor/puck/light', getLight());
+  }
+  if(ENABLE_MAGNETOMETER) {
+    MQTT_publish('home-assistant/sensor/puck/mag', getMagnetometer());
+  }
 }
 
 var on = 0;
 
 var presses = 0;
-NRF.setAdvertising({},{manufacturer: 0x0590, manufacturerData:[presses]});
-
 // publish events on button press
 setWatch(function() {
   on = !on;
@@ -91,7 +75,3 @@ LED3.set();
 setTimeout(function() {
         LED3.reset();
 },1000);
-
-
-
-
