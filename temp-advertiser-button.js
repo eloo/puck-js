94671249bb2b3c3e7bb5d08d3e710e06db632ed1
Joseph Weigl <2631895+eloo@users.noreply.github.com>
2018-11-28 22:08:55 +0100

    Update temp-advertiser-button.js

--- temp-advertiser-button.js
+++ temp-advertiser-button.js
@@ -1,15 +1,22 @@
 function updateAdv() {
-      digitalPulse(LED1, true, 200);
-      var data = [E.getBattery(), E.getTemperature()];
-      NRF.setAdvertising({},{manufacturer: 0x0590, manufacturerData:data});
-      // console.log(data); // for debugging
+//      digitalPulse(LED1, true, 200);
+//      var data = [E.getBattery(), E.getTemperature()];
+//      var data = E.getBattery();
+//      var sensor_data = {};
+//      sensor_data.temp = E.getTemperature();
+//      sensor_data.batt = E.getBattery();
+      NRF.setAdvertising({
+          0x180f : [Puck.getBatteryPercentage()],
+          0x1809 : [E.getTemperature()]
+        });
+//      console.log(data); // for debugging
     }
 
 updateAdv(); // start off right now
 setInterval(updateAdv, 60000); // update every 60 seconds
 
 
-setWatch(function(e) {
+/*setWatch(function(e) {
   
   var len = e.time - e.lastTime;
   if (len > 0.3) {
@@ -20,3 +27,5 @@ setWatch(function(e) {
     digitalPulse(LED2,1,100);
   }
 }, BTN, { edge:"falling",repeat:true,debounce:50});
+*/
+console.log("Flashed");
